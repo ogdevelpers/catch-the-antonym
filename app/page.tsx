0a93d67a4609199dd9c4e80a6f7689718b1e1668
og developers <developers506@gmail.com>
2025-12-11 16:12:12 +0530

    Convert to SSG

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import AntonymGame from './components/AntonymGame';
 
+// Force static generation
+export const dynamic = 'force-static';
+
 export default function Home() {
   return <AntonymGame />;
 }
